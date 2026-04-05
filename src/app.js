d0b7a6fd8b8ca2aafcfbbe9a95d885cd15e3a9af
Douglas Tarazone <doutarazona09@gmail.com>
2026-04-05 15:58:43 -0400

    add CORS in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import cors from 'cors';
 import express from 'express';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,10 @@ import { runServer } from './server.js';
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
